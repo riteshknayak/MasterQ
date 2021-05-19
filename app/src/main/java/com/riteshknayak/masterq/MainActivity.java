110cf1dc68160bfad08c3920ed1bcefc5970f24d
Ritesh <ritesh.kumar.nayak.28.10.2005@gmail.com>
2021-05-19 17:14:49 +0530

    Added Bottom Bar

--- app/src/main/java/com/riteshknayak/masterq/MainActivity.java
+++ app/src/main/java/com/riteshknayak/masterq/MainActivity.java
@@ -1,14 +1,53 @@
 package com.riteshknayak.masterq;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.riteshknayak.masterq.databinding.ActivityMainBinding;
+
+
+import me.ibrahimsn.lib.OnItemSelectedListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content, new HomeFragment());
+        transaction.commit();
+
+        binding.bottomBar.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public boolean onItemSelect(int i) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                switch (i) {
+                    case 0:
+                        transaction.replace(R.id.content, new HomeFragment());
+                        transaction.commit();
+                        break;
+                    case 1:
+                        transaction.replace(R.id.content, new LeaderboardFragment());
+                        transaction.commit();
+                        break;
+                    case 2:
+                        transaction.replace(R.id.content, new ProfileFragment());
+                        transaction.commit();
+                        break;
+                }
+                return false;
+            }
+        });
+
     }
 }
