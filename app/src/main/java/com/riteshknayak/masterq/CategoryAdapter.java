7132e091e53c8ad6957cabe9387432e062172927
Ritesh <ritesh.kumar.nayak.28.10.2005@gmail.com>
2021-05-21 18:10:25 +0530

    checking category data weather the category is unlocked

--- app/src/main/java/com/riteshknayak/masterq/CategoryAdapter.java
+++ app/src/main/java/com/riteshknayak/masterq/CategoryAdapter.java
@@ -2,7 +2,6 @@ package com.riteshknayak.masterq;
 
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +13,9 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
@@ -57,28 +56,47 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
 
         //TODO  below onClickListener for opening quizActivity
         holder.itemView.setOnClickListener(v -> {
-            Intent intent = new Intent(context, QuizActivity.class);
-            intent.putExtra("catId", model.getCategoryId());
-            context.startActivity(intent);
-            Map<String, Boolean> data = new HashMap<>();
-            data.put(model.getCategoryId(), true);
+
+            //below code to check if  category is unlocked
+            final boolean[] val = new boolean[1];
             database.collection("users")
-                    .document(FirebaseAuth.getInstance().getUid())
+                    .document(Objects.requireNonNull(FirebaseAuth.getInstance().getUid()))
                     .collection("userData")
                     .document("categories")
-                    .set(data)
-                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            Log.d(null, "DocumentSnapshot successfully written!");
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.w(null, "Error writing document", e);
-                        }
-                    });
+                    .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    val[0] = documentSnapshot.getBoolean(model.getCategoryId());
+                }
+            });
+            if(val[0]){
+                Intent intent = new Intent(context, QuizActivity.class);
+                intent.putExtra("catId", model.getCategoryId());
+                context.startActivity(intent);
+                Map<String, Boolean> data = new HashMap<>();
+                data.put(model.getCategoryId(), true);
+            }else{
+                Toast toast = Toast.makeText(context, "Access Denied", Toast.LENGTH_LONG);
+                toast.show();
+            }
+//             add category true or false
+//            database.collection("users")
+//                    .document(FirebaseAuth.getInstance().getUid())
+//                    .collection("userData")
+//                    .document("categories")
+//                    .set(data)
+//                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                        @Override
+//                        public void onSuccess(Void aVoid) {
+//                            Log.d(null, "DocumentSnapshot successfully written!");
+//                        }
+//                    })
+//                    .addOnFailureListener(new OnFailureListener() {
+//                        @Override
+//                        public void onFailure(@NonNull Exception e) {
+//                            Log.w(null, "Error writing document", e);
+//                        }
+//                    });
         });
     }
 
