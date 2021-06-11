b2b3100044006916b6030dbf34a1dcd6c292e4cc
Ritesh <ritesh.kumar.nayak.28.10.2005@gmail.com>
2021-06-11 18:08:58 +0530

    Optimized code of activity_login.xml and activity_signup.xml

--- app/src/main/java/com/riteshknayak/masterq/LoginActivity.java
+++ app/src/main/java/com/riteshknayak/masterq/LoginActivity.java
@@ -1,11 +1,8 @@
 package com.riteshknayak.masterq;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,8 +11,6 @@ import com.google.firebase.appcheck.FirebaseAppCheck;
 import com.google.firebase.appcheck.safetynet.SafetyNetAppCheckProviderFactory;
 import com.google.firebase.auth.FirebaseAuth;
 import com.riteshknayak.masterq.databinding.ActivityLoginBinding;
-
-import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -35,34 +30,33 @@ public class LoginActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
 
-        //TODO app crashing when  clicking the sign in button when edittext is empty . use try-catch to solve the error
-        //TODO Check weather the quizme app also crash when empty edittext is clicked
+        //TODO isEmpty(binding.emailBox) ||
 
-        binding.emailBtn.setOnClickListener(v -> {
-            if (isEmpty(binding.emailBox) || isEmpty(binding.passwordBox) ) {
-                Toast.makeText(LoginActivity.this, "Please provide the required data", Toast.LENGTH_SHORT).show();
-            } else {
-
-                String email, pass;
-                email = binding.emailBox.getText().toString();
-                pass = binding.passwordBox.getText().toString();
-
-
-                auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                        finish();
-                        SharedPreferences shared = getSharedPreferences("app", Context.MODE_PRIVATE);
-                        SharedPreferences.Editor editor = shared.edit();
-                        editor.putString("UId", auth.getCurrentUser().getUid());
-                        editor.apply();
-
-                    } else {
-                        Toast.makeText(LoginActivity.this, Objects.requireNonNull(task.getException()).getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
+//        binding.emailBtn.setOnClickListener(v -> {
+//            if ( isEmpty(binding.passwordBox) ) {
+//                Toast.makeText(LoginActivity.this, "Please provide the required data", Toast.LENGTH_SHORT).show();
+//            } else {
+//
+//                String email, pass;
+////                email = binding.emailBox.getText().toString();
+//                pass = binding.passwordBox.getText().toString();
+//
+//
+//                auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(task -> {
+//                    if (task.isSuccessful()) {
+//                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+//                        finish();
+//                        SharedPreferences shared = getSharedPreferences("app", Context.MODE_PRIVATE);
+//                        SharedPreferences.Editor editor = shared.edit();
+//                        editor.putString("UId", auth.getCurrentUser().getUid());
+//                        editor.apply();
+//
+//                    } else {
+//                        Toast.makeText(LoginActivity.this, Objects.requireNonNull(task.getException()).getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//            }
+//        });
 
         binding.createBtn.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, SignupActivity.class)));
 
